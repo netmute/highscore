6fcddbeb852eded1cff5b97aaadc9df5e3da2b55
Simon Ernst <se@netmute.org>
2012-05-02 17:48:01 +0200

    Initial commit

---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'bundler'
+Bundler.require
+
+require './highscore'
+run Sinatra::Application
---
+++ highscore.rb
@@ -0,0 +1,25 @@
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/highscore.db")
+
+class Highscore
+    include DataMapper::Resource
+    property :id, Serial
+    property :name, String
+    property :mode, String
+    property :score, String
+    property :created_at, DateTime
+end
+
+DataMapper.finalize
+Highscore.auto_upgrade!
+
+get '/' do
+  content_type :json
+  @highscores = Highscore.all(order: :score.asc, limit: 10)
+  @highscores.to_json
+end
+
+post '/' do
+  content_type :json
+  @highscore = Highscore.create(params)
+  @highscore.to_json
+end
